e64700d983cac63200687886b044b7f49b133172
Shreejay Mane <shreejaymane15@gmail.com>
2023-11-28 11:51:04 +0530

    Pong Complete

--- Pong/main.lua
+++ Pong/main.lua
@@ -46,14 +46,33 @@ function love.load()
     -- set Love2D active font to small font object
     love.graphics.setFont(smallFont)
     
+
+    -- set up our sound effects; later, we can just index this table end call each entry's `play` method
+    sounds = {
+        ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static'),
+        ['win'] = love.audio.newSource('sounds/win.wav', 'static')
+    }
+
     -- initialize virtual resolution, which will be rendered within our actual window no matter its dimensions
     -- replace our love.window.setMode from last example
 
+    -- push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    --     fullscreen = false,
+    --     resizable = false,
+    --     vsync = true
+    -- })
+
+    -- set resize = true
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-        resizable = false,
+        resizable = true,
         vsync = true
     })
+
+
 
     --initialize score variables, used for rendering on the screen and keeping track of winner
     player1Score = 0
@@ -92,6 +111,16 @@ function love.load()
     gameState  = 'start'
 end
 
+
+
+--[[
+    Called by LOVE whenever we resize the screen; here, we just want to pass it's width and height 
+    to push so our virtual resolution can be resized as needed
+]]
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
 --[[Runs every frame, with "dt" passed in, our delta in seconds since the last frame,
     Which LOVE2D supplies us
   ]]
@@ -121,6 +150,8 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 150)
             end
+
+            sounds['paddle_hit']:play()
         end
 
         if ball:collides(player2) then
@@ -133,18 +164,22 @@ function love.update(dt)
             else    
                 ball.dy = math.random(10, 150)
             end
+
+            sounds['paddle_hit']:play()
         end
 
         -- detect upper and lower screen boundry collision and reverse if collision happened
         if ball.y <= 0 then
             ball.y = 0
             ball.dy = -ball.dy
+            sounds['wall_hit']:play()
         end
 
         -- -4 to account for ball size 
         if ball.y >= VIRTUAL_HEIGHT - 4 then
             ball.y = VIRTUAL_HEIGHT - 4
             ball.dy = - ball.dy
+            sounds['wall_hit']:play()
         end
         
         -- if we reach the left or right edge of the screen,
@@ -153,11 +188,13 @@ function love.update(dt)
         if ball.x < 0 then
             servingPlayer = 1
             player2Score = player2Score + 1
-            
+            sounds['score']:play()    
             -- if we've reached a score of 10, the game is over; set the state to done so we can show the victory message
             if player2Score == 10 then
                 winningPlayer = 2
                 gameState = 'done'
+                sounds['win']:setLooping(true)
+                sounds['win']:play()
             else
                 gameState = 'serve'
                 ball:reset()
@@ -167,10 +204,13 @@ function love.update(dt)
         if ball.x > VIRTUAL_WIDTH then
             servingPlayer = 2
             player1Score = player1Score + 1
-            
+            sounds['score']:play()    
+
             if player1Score == 10 then
                 winningPlayer = 1
                 gameState = 'done'
+                sounds['win']:setLooping(true)
+                sounds['win']:play()
             else
                 gameState = 'serve'
                 ball:reset()
@@ -285,7 +325,7 @@ function love.keypressed(key)
             ball:reset()
             player1Score = 0
             player2Score = 0
-
+            sounds['win']:stop()
             if winningPlayer == 1 then
                 servingPlayer = 2
             else
