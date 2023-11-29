47db9b15a8a42faeb395ae8fab20d5532c3db37c
Shreejay Mane <shreejaymane15@gmail.com>
2023-11-29 11:28:18 +0530

    Flappy Bird Start

---
+++ Flappy Bird/main.lua
@@ -0,0 +1,68 @@
+-- import push library 
+push = require 'push'
+
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+
+-- images we load into memory from files to later draw onto the screen
+local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
+
+local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
+
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
+
+
+
+function love:load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.window.setTitle('Flappy Bird')
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
+end
+
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+
+function love.update(dt)
+
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+
+end
+
+
+
+function love.draw()
+    push:start()
+
+    love.graphics.draw(background, -backgroundScroll, 0)
+
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+   
+    push:finish()
+end
